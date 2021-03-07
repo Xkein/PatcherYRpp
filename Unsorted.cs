499abf266b18bcdb6bc6e702e8439b5187eabb6a
Xkein <739502808@qq.com>
2021-03-07 15:22:18 +0800

    add file Unsorted.cs.

---
+++ Unsorted.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatcherYRpp
+{
+    static class Game
+    {
+        public static int CurrentFrame { get => pCurrentFrame.Convert<int>().Data; set => pCurrentFrame.Convert<int>().Ref = value; }
+        private static IntPtr pCurrentFrame = new IntPtr(0xA8ED84);
+
+        // The height in the middle of a cell with a slope of 30 degrees
+        public const int LevelHeight = 104;//89DE70
+        public const int BridgeHeight = LevelHeight * 4;//ABC5DC
+    }
+}
