65d7544d274788990c64ef0fd713ebda5c6ae3d8
Xkein <739502808@qq.com>
2021-03-07 15:16:47 +0800

    add function AbstractClass.WhatAmI().

--- AbstractClass.cs
+++ AbstractClass.cs
@@ -7,26 +7,31 @@ using System.Threading.Tasks;
 
 namespace PatcherYRpp
 {
+    [StructLayout(LayoutKind.Explicit, Size = 36)]
+    public struct AbstractClass
+    {
+        public unsafe AbstractType WhatAmI()
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref AbstractClass, AbstractType>)Helpers.GetVirtualFunctionPointer(Pointer<AbstractClass>.AsPointer(ref this), 11);
+            return func(ref this);
+        }
 
-	[StructLayout(LayoutKind.Explicit, Size = 36)]
-	public struct AbstractClass
-	{
-		public unsafe int GetArrayIndex()
-		{
-			var func = (delegate* unmanaged[Thiscall]<ref AbstractClass, int>)Helpers.GetVirtualFunctionPointer(Pointer<AbstractClass>.AsPointer(ref this), 16);
-			return func(ref this);
-		}
+        public unsafe int GetArrayIndex()
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref AbstractClass, int>)Helpers.GetVirtualFunctionPointer(Pointer<AbstractClass>.AsPointer(ref this), 16);
+            return func(ref this);
+        }
 
-		public unsafe CoordStruct GetCoords()
-		{
-			var func = (delegate* unmanaged[Thiscall]<ref AbstractClass, IntPtr, IntPtr>)Helpers.GetVirtualFunctionPointer(Pointer<AbstractClass>.AsPointer(ref this), 18);
+        public unsafe CoordStruct GetCoords()
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref AbstractClass, IntPtr, IntPtr>)Helpers.GetVirtualFunctionPointer(Pointer<AbstractClass>.AsPointer(ref this), 18);
 
-			CoordStruct ret = default;
-			func(ref this, Pointer<CoordStruct>.AsPointer(ref ret));
-			return ret;
-		}
+            CoordStruct ret = default;
+            func(ref this, Pointer<CoordStruct>.AsPointer(ref ret));
+            return ret;
+        }
 
-		[FieldOffset(0)]
-		public int Vfptr;
-	}
+        [FieldOffset(0)]
+        public int Vfptr;
+    }
 }
