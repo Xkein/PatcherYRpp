5b03e42677f610127aad03a52355dba9eaa3620d
Xkein <739502808@qq.com>
2021-03-09 00:40:29 +0800

    remove DamageArea and FlashbangWarheadAt from MapClass.

--- MapClass.cs
+++ MapClass.cs
@@ -11,29 +11,30 @@ namespace PatcherYRpp
     [StructLayout(LayoutKind.Explicit, Size = 4468)]
     public struct MapClass
     {
+        // no fast call. unmanaged call will lead to StackOverflowException.
         //[UnmanagedFunctionPointer(CallingConvention.FastCall)]
         //public delegate DamageAreaResult DamageAreaFunction(in CoordStruct Coords, int Damage, /*Pointer<TechnoClass>*/IntPtr SourceObject,
         //    IntPtr WH, bool AffectsTiberium, IntPtr SourceHouse);
         //public static DamageAreaFunction DamageAreaDlg = Marshal.GetDelegateForFunctionPointer<DamageAreaFunction>(new IntPtr(0x489280));
 
-        public static unsafe DamageAreaResult DamageArea(in CoordStruct Coords, int Damage, Pointer<TechnoClass> SourceObject,
-            Pointer<WarheadTypeClass> WH, bool AffectsTiberium, Pointer<HouseClass> SourceHouse)
-        {
-            //var func = (delegate* unmanaged[Fastcall]<in CoordStruct, int, IntPtr, IntPtr, bool, IntPtr, DamageAreaResult>)0x489280;
-            var func = (delegate* managed<in CoordStruct, int, Pointer<HouseClass>, bool, Pointer<WarheadTypeClass>, Pointer<TechnoClass>, DamageAreaResult>)0x489280;
-            return func(in Coords, Damage, SourceHouse, AffectsTiberium, WH, SourceObject);
-        }
+        //public static unsafe DamageAreaResult DamageArea(in CoordStruct Coords, int Damage, Pointer<TechnoClass> SourceObject,
+        //    Pointer<WarheadTypeClass> WH, bool AffectsTiberium, Pointer<HouseClass> SourceHouse)
+        //{
+        //    //var func = (delegate* unmanaged[Fastcall]<in CoordStruct, int, IntPtr, IntPtr, bool, IntPtr, DamageAreaResult>)0x489280;
+        //    var func = (delegate* managed<in CoordStruct, int, Pointer<HouseClass>, bool, Pointer<WarheadTypeClass>, Pointer<TechnoClass>, DamageAreaResult>)0x489280;
+        //    return func(in Coords, Damage, SourceHouse, AffectsTiberium, WH, SourceObject);
+        //}
 
         //[UnmanagedFunctionPointer(CallingConvention.FastCall)]
         //public delegate void FlashbangWarheadAtFunction(int Damage, IntPtr WH, CoordStruct coords, bool Force = false, SpotlightFlags CLDisableFlags = SpotlightFlags.None);
         //public static FlashbangWarheadAtFunction FlashbangWarheadAt = Marshal.GetDelegateForFunctionPointer<FlashbangWarheadAtFunction>(new IntPtr(0x48A620));
 
-        public static unsafe void FlashbangWarheadAt(int Damage, Pointer<WarheadTypeClass> WH, CoordStruct coords, bool Force = false, SpotlightFlags CLDisableFlags = SpotlightFlags.None)
-        {
-            //var func = (delegate* unmanaged[Fastcall]<int, IntPtr, CoordStruct, bool, SpotlightFlags, void>)0x48A620;
-            var func = (delegate* managed<int, Pointer<WarheadTypeClass>, SpotlightFlags, bool, CoordStruct, void>)0x48A620;
-            func(Damage, WH, CLDisableFlags, Force, coords);
-        }
+        //public static unsafe void FlashbangWarheadAt(int Damage, Pointer<WarheadTypeClass> WH, CoordStruct coords, bool Force = false, SpotlightFlags CLDisableFlags = SpotlightFlags.None)
+        //{
+        //    //var func = (delegate* unmanaged[Fastcall]<int, IntPtr, CoordStruct, bool, SpotlightFlags, void>)0x48A620;
+        //    var func = (delegate* managed<int, Pointer<WarheadTypeClass>, SpotlightFlags, bool, CoordStruct, void>)0x48A620;
+        //    func(Damage, WH, CLDisableFlags, Force, coords);
+        //}
 
 
         public static CoordStruct Cell2Coord(CellStruct cell, int z = 0)
