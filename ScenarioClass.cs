be84192207a9aa57e0a9d49df255d21515a8f7ce
Xkein <739502808@qq.com>
2021-04-24 23:54:27 +0800

    add ScenarioClass.

---
+++ ScenarioClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatcherYRpp
+{
+    [StructLayout(LayoutKind.Explicit, Size = 41)]
+    public struct Variable
+    {
+        [FieldOffset(0)] public byte Name_first;
+        public AnsiString Name => Pointer<byte>.AsPointer(ref Name_first);
+        [FieldOffset(40)] public byte Value;
+    };
+
+    [StructLayout(LayoutKind.Explicit, Size = 14144)]
+    public struct ScenarioClass
+    {
+        private static IntPtr instance = new IntPtr(0xA8B230);
+        static public ref ScenarioClass Instance { get => ref instance.Convert<Pointer<ScenarioClass>>().Ref.Ref; }
+
+
+        [FieldOffset(4700)] public byte FileName_first;
+        public AnsiString GetFileName() => Pointer<byte>.AsPointer(ref FileName_first);
+
+        [FieldOffset(7304)] public Variable GlobalVariables_first;
+        public Pointer<Variable> GlobalVariables => Pointer<Variable>.AsPointer(ref GlobalVariables_first);
+        [FieldOffset(9354)] public Variable LocalVariables_first;
+        public Pointer<Variable> LocalVariables => Pointer<Variable>.AsPointer(ref LocalVariables_first);
+    }
+}
