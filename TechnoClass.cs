6878247b9b14f1fb10d3ea89aa9e489b6c585656
Xkein <739502808@qq.com>
2021-06-30 22:32:38 +0800

    update TechnoClass and TechnoTypeClass.

--- TechnoClass.cs
+++ TechnoClass.cs
@@ -7,6 +7,95 @@ using System.Threading.Tasks;
 
 namespace PatcherYRpp
 {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PassengersClass
+    {
+        public int NumPassengers;
+        public IntPtr firstPassenger;
+        public Pointer<FootClass> FirstPassenger { get => firstPassenger; set => firstPassenger = value; }
+
+        public unsafe void AddPassenger(Pointer<FootClass> pPassenger)
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref PassengersClass, IntPtr, void>)0x4733A0;
+            func(ref this, pPassenger);
+        }
+
+        public unsafe Pointer<FootClass> GetFirstPassenger()
+        {
+            return this.FirstPassenger;
+        }
+
+        public unsafe Pointer<FootClass> RemoveFirstPassenger()
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref PassengersClass, IntPtr>)0x473430;
+            return func(ref this);
+        }
+
+        public unsafe int GetTotalSize()
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref PassengersClass, int>)0x473460;
+            return func(ref this);
+        }
+
+        public unsafe int IndexOf(Pointer<FootClass> candidate)
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref PassengersClass, IntPtr, int>)0x473500;
+            return func(ref this, candidate);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FlashData
+    {
+        public int DurationRemaining;
+        public Bool FlashingNow;
+
+        public unsafe bool Update()
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref FlashData, Bool>)0x4CC770;
+            return func(ref this);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct OwnedTiberiumStruct
+    {
+        public unsafe float GetAmount(int index)
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref OwnedTiberiumStruct, int, float>)0x6C9680;
+            return func(ref this, index);
+        }
+
+        public unsafe float GetTotalAmount()
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref OwnedTiberiumStruct, float>)0x6C9650;
+            return func(ref this);
+        }
+
+        public unsafe float AddAmount(float amount, int index)
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref OwnedTiberiumStruct, float, int, float>)0x6C9690;
+            return func(ref this, amount, index);
+        }
+
+        public unsafe float RemoveAmount(float amount, int index)
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref OwnedTiberiumStruct, float, int, float>)0x6C96B0;
+            return func(ref this, amount, index);
+        }
+
+        public unsafe int GetTotalValue()
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref OwnedTiberiumStruct, int>)0x6C9600;
+            return func(ref this);
+        }
+
+        public float Tiberium1;
+        public float Tiberium2;
+        public float Tiberium3;
+        public float Tiberium4;
+    }
+
     [StructLayout(LayoutKind.Explicit, Size = 1312)]
     public struct TechnoClass
     {
@@ -22,14 +111,57 @@ namespace PatcherYRpp
             }
         }
 
-        [FieldOffset(0)]  public ObjectClass Base;
 
-        [FieldOffset(540)] private IntPtr owner;
+        public unsafe int SelectWeapon(Pointer<AbstractClass> pTarget)
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref TechnoClass, IntPtr, int>)
+                Helpers.GetVirtualFunctionPointer(Pointer<TechnoClass>.AsPointer(ref this), 185);
+            return func(ref this, pTarget);
+        }
+
+        public unsafe int GetROF(int idxWeapon)
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref TechnoClass, int, int>)
+                Helpers.GetVirtualFunctionPointer(Pointer<TechnoClass>.AsPointer(ref this), 198);
+            return func(ref this, idxWeapon);
+        }
+
+        public unsafe Pointer<WeaponStruct> GetWeapon(int idxWeapon)
+        {
+            var func = (delegate* unmanaged[Thiscall]<ref TechnoClass, int, IntPtr>)
+                Helpers.GetVirtualFunctionPointer(Pointer<TechnoClass>.AsPointer(ref this), 254);
+            return func(ref this, idxWeapon);
+        }
+
+        [FieldOffset(0)] public ObjectClass Base;
+
+
+        [FieldOffset(240)] public FlashData Flashing;
+        [FieldOffset(248)] public ProgressTimer Animation; // how the unit animates
+        [FieldOffset(276)] public PassengersClass Passengers;
+
+        [FieldOffset(460)] public IntPtr drainTarget; // eg Disk -> PowerPlant, this points to PowerPlant
+        public Pointer<TechnoClass> DrainTarget { get => drainTarget; set => drainTarget = value; }
+        [FieldOffset(464)] public IntPtr drainingMe; // eg Disk -> PowerPlant, this points to Disk
+        public Pointer<TechnoClass> DrainingMe { get => drainingMe; set => drainingMe = value; }
+        [FieldOffset(468)] public IntPtr drainAnim;
+        public Pointer<AnimClass> DrainAnim { get => drainAnim; set => drainAnim = value; }
+
+        [FieldOffset(540)] public IntPtr owner;
         public Pointer<HouseClass> Owner { get => owner; set => owner = value; }
 
         [FieldOffset(664)] public Bool Berzerk;
 
         [FieldOffset(692)] public Pointer<AbstractClass> Target; //if attacking
         [FieldOffset(696)] public Pointer<AbstractClass> LastTarget;
+
+        [FieldOffset(764)] public int Ammo;
+
+        [FieldOffset(828)] public OwnedTiberiumStruct Tiberium;
+
+        [FieldOffset(880)] public FacingStruct BarrelFacing;
+        [FieldOffset(904)] public FacingStruct Facing;
+        [FieldOffset(928)] public FacingStruct TurretFacing;
+        [FieldOffset(952)] public int CurrentBurstIndex;
     }
 }
--- TechnoTypeClass.cs
+++ TechnoTypeClass.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 
 namespace PatcherYRpp
 {
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class WeaponStruct
+    {
+        public IntPtr weaponType;
+		public Pointer<WeaponTypeClass> WeaponType { get => weaponType; set => weaponType = value; }
+        public CoordStruct FLH;
+        public int BarrelLength;
+        public int BarrelThickness;
+        public bool TurretLocked;
+    }
+
     [StructLayout(LayoutKind.Explicit, Size = 3576)]
     public struct TechnoTypeClass
     {
@@ -16,5 +28,11 @@ namespace PatcherYRpp
 
         [FieldOffset(0)]
         public ObjectTypeClass Base;
+
+        [FieldOffset(2200)] public WeaponStruct Weapon_first;
+        public Pointer<WeaponStruct> Weapon => Pointer<WeaponStruct>.AsPointer(ref Weapon_first);
+        [FieldOffset(2704)] public Bool ClearAllWeapons;
+        [FieldOffset(2708)] public WeaponStruct EliteWeapon_first;
+        public Pointer<WeaponStruct> EliteWeapon => Pointer<WeaponStruct>.AsPointer(ref EliteWeapon_first);
     }
 }
