a6bcaf93983a6e965420e07d60e36947408a6836
Xkein <739502808@qq.com>
2021-06-30 21:08:43 +0800

    update BulletClass.

--- BulletClass.cs
+++ BulletClass.cs
@@ -56,34 +56,30 @@ namespace PatcherYRpp
         [FieldOffset(0)]
         public ObjectClass Base;
 
-        [FieldOffset(172)]
-        public Pointer<BulletTypeClass> Type;
-        [FieldOffset(176)]
-        public Pointer<TechnoClass> Owner;
+        [FieldOffset(172)] public Pointer<BulletTypeClass> Type;
+        [FieldOffset(176)] public Pointer<TechnoClass> Owner;
 
-        [FieldOffset(184)]
-        public BulletData Data;
-        [FieldOffset(224)]
-        public byte Bright;
+        [FieldOffset(184)] public BulletData Data;
+        [FieldOffset(224)] public Bool Bright;
 
-        [FieldOffset(232)]
-        public BulletVelocity Velocity;
+        [FieldOffset(232)] public BulletVelocity Velocity;
 
-        [FieldOffset(268)]
-        public Pointer<AbstractClass> Target;
-        [FieldOffset(272)]
-        public int Speed;
+        [FieldOffset(261)] public Bool CourseLocked;
+        [FieldOffset(264)] public int CourseLockedDuration;
+        [FieldOffset(268)] public Pointer<AbstractClass> Target;
+        [FieldOffset(272)] public int Speed;
+        [FieldOffset(276)] public int InheritedColor;
 
-        [FieldOffset(296)]
-        public Pointer<WarheadTypeClass> WH;
-
-        [FieldOffset(308)]
-        public CoordStruct SourceCoords;
-        [FieldOffset(320)]
-        public CoordStruct TargetCoords;
-        [FieldOffset(332)]
-        public CellStruct LastMapCoords;
-        [FieldOffset(336)]
-        public int DamageMultiplier;
+        [FieldOffset(296)] public Pointer<WarheadTypeClass> WH;
+        [FieldOffset(300)] public byte AnimFrame;
+        [FieldOffset(301)] public byte AnimRateCounter;
+        [FieldOffset(304)] public Pointer<WeaponTypeClass> WeaponType;
+        [FieldOffset(308)] public CoordStruct SourceCoords;
+        [FieldOffset(320)] public CoordStruct TargetCoords;
+        [FieldOffset(332)] public CellStruct LastMapCoords;
+        [FieldOffset(336)] public int DamageMultiplier;
+        [FieldOffset(340)] public Pointer<AnimClass> NextAnim;
+        [FieldOffset(344)] public Bool SpawnNextAnim;
+        [FieldOffset(348)] public int Range;
     }
 }
