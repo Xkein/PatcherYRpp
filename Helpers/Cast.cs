e677dd0c5e40cd45a137b3687c1c3aec66f9f564
Xkein <739502808@qq.com>
2022-01-22 03:48:51 +0800

    add CastToObject, CastToFoot in Cast.cs

--- Helpers/Cast.cs
+++ Helpers/Cast.cs
@@ -10,14 +10,10 @@ namespace PatcherYRpp
     {
         public static bool CastToTechno(this Pointer<AbstractClass> pAbstract, out Pointer<TechnoClass> pTechno)
         {
-            switch (pAbstract.Ref.WhatAmI())
+            if (pAbstract.Ref.AbstractFlags.HasFlag(AbstractFlags.Techno))
             {
-                case AbstractType.Unit:
-                case AbstractType.Aircraft:
-                case AbstractType.Building:
-                case AbstractType.Infantry:
-                    pTechno = pAbstract.Convert<TechnoClass>();
-                    return true;
+                pTechno = pAbstract.Convert<TechnoClass>();
+                return true;
             }
             pTechno = Pointer<TechnoClass>.Zero;
             return false;
@@ -25,6 +21,36 @@ namespace PatcherYRpp
         public static bool CastToTechno(this Pointer<ObjectClass> pObject, out Pointer<TechnoClass> pTechno)
         {
             return pObject.Convert<AbstractClass>().CastToTechno(out pTechno);
+        }
+
+        public static bool CastToObject(this Pointer<AbstractClass> pAbstract, out Pointer<ObjectClass> pObject)
+        {
+            if (pAbstract.Ref.AbstractFlags.HasFlag(AbstractFlags.Object))
+            {
+                pObject = pAbstract.Convert<ObjectClass>();
+                return true;
+            }
+            pObject = Pointer<ObjectClass>.Zero;
+            return false;
+        }
+
+        public static bool CastToFoot(this Pointer<AbstractClass> pAbstract, out Pointer<FootClass> pFoot)
+        {
+            if (pAbstract.Ref.AbstractFlags.HasFlag(AbstractFlags.Foot))
+            {
+                pFoot = pAbstract.Convert<FootClass>();
+                return true;
+            }
+            pFoot = Pointer<FootClass>.Zero;
+            return false;
+        }
+        public static bool CastToFoot(this Pointer<ObjectClass> pObject, out Pointer<FootClass> pFoot)
+        {
+            return pObject.Convert<AbstractClass>().CastToFoot(out pFoot);
+        }
+        public static bool CastToFoot(this Pointer<TechnoClass> pTechno, out Pointer<FootClass> pFoot)
+        {
+            return pTechno.Convert<AbstractClass>().CastToFoot(out pFoot);
         }
     }
 }
