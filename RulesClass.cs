6c9cb24a4ac440dded58b8561a0229f24060bd88
ChrisLv <12737@live.com>
2022-09-15 00:42:51 +0700

    重要的全局对象

--- RulesClass.cs
+++ RulesClass.cs
@@ -284,6 +284,11 @@ namespace PatcherYRpp
             func(ref this, pINI);
         }
 
+        public static RulesClass Global()
+        {
+            return Instance.Ref;
+        }
+
         [FieldOffset(1640)] public double VeteranRatio;
         [FieldOffset(1648)] public double VeteranCombat;
         [FieldOffset(1656)] public double VeteranSpeed;
@@ -291,11 +296,42 @@ namespace PatcherYRpp
         [FieldOffset(1672)] public double VeteranArmor;
         [FieldOffset(1680)] public double VeteranROF;
         [FieldOffset(1688)] public double VeteranCap;
-
-
+        [FieldOffset(3008)] public DynamicVectorClass<AnimTypeClass> SplashList;
+        [FieldOffset(3972)] public Pointer<WarheadTypeClass> FlameDamage;
+        [FieldOffset(3976)] public Pointer<WarheadTypeClass> FlameDamage2;
+        [FieldOffset(3980)] public Pointer<WarheadTypeClass> NukeWarhead;
+        [FieldOffset(3984)] public Pointer<BulletTypeClass> NukeProjectile;
+        [FieldOffset(3988)] public Pointer<BulletTypeClass> NukeDown;
+        [FieldOffset(4008)] public Pointer<WarheadTypeClass> C4Warhead;
+        [FieldOffset(4012)] public Pointer<WarheadTypeClass> CrushWarhead;
         [FieldOffset(5432)] public DifficultyStruct Easy;
         [FieldOffset(5512)] public DifficultyStruct Normal;
         [FieldOffset(5592)] public DifficultyStruct Difficult;
+        [FieldOffset(5816)] public int Gravity;
+        [FieldOffset(5832)] public int MaxDamage;
+        [FieldOffset(5940)] public int BallisticScatter; // value in *256
+        [FieldOffset(6148)] public int RadDurationMultiple;
+        [FieldOffset(6152)] public int RadApplicationDelay;
+        [FieldOffset(6156)] public int RadLevelMax;
+        [FieldOffset(6160)] public int RadLevelDelay;
+        [FieldOffset(6164)] public int RadLightDelay;
+        [FieldOffset(6168)] public double RadLevelFactor;
+        [FieldOffset(6176)] public double RadLightFactor;
+        [FieldOffset(6184)] public double RadTintFactor;
+        [FieldOffset(6192)] public ColorStruct RadColor;
+        [FieldOffset(6196)] public Pointer<WarheadTypeClass> RadSiteWarhead;
+        [FieldOffset(6246)] public ColorStruct ChronoBeamColor;
+        [FieldOffset(6249)] public ColorStruct MagnaBeamColor;
+        [FieldOffset(6260)] public ColorStruct colorAdd_first;
+        public Pointer<ColorStruct> ColorAdd => Pointer<ColorStruct>.AsPointer(ref colorAdd_first);
+        [FieldOffset(6308)] public int laserTargetColor;
+        public static ColorStruct LaserTargetColor => Instance.Ref.ColorAdd[Instance.Ref.laserTargetColor];
+        [FieldOffset(6312)] public int ironCurtainColor;
+        public static ColorStruct IronCurtainColor => Instance.Ref.ColorAdd[Instance.Ref.ironCurtainColor];
+        [FieldOffset(6316)] public int berserkColor;
+        public static ColorStruct BerserkColor => Instance.Ref.ColorAdd[Instance.Ref.berserkColor];
+        [FieldOffset(6320)] public int forceShieldColor;
+        public static ColorStruct ForceShieldColor => Instance.Ref.ColorAdd[Instance.Ref.forceShieldColor];
 
     }
 }
